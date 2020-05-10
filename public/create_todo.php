2ee4e0e3e58d0735fa1489c59bc3fdd70d8f0058
Jeremy-Hoarau <292627@supinfo.com>
2020-05-10 12:45:48 +0400

    "previous page" button

--- public/create_todo.php
+++ public/create_todo.php
@@ -23,6 +23,16 @@ if(is_post_request())
 }
 ?>
 
+<!-- Previous Page Button -->
+<a href="<?php echo url_for('/index.php') ?>">
+    <button class="btn back-color-0 border-color-0 color-4" style="position: absolute; margin: 20px">
+        <svg class="bi bi-arrow-left" width="2em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clip-rule="evenodd"/>
+            <path fill-rule="evenodd" d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clip-rule="evenodd"/>
+        </svg>
+    </button>
+</a>
+
 <div style="margin-top:60px">
     <div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
